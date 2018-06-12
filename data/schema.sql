d9e2e0fa3e02289f8f822f90b2af9300a288d38e
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-06-12 11:55:48 +0300

    Removed ZF from the repository

--- data/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE album (`id` INTEGER PRIMARY KEY AUTO_INCREMENT, `artist` varchar(100) NOT NULL, `title` varchar(100) NOT NULL);
-INSERT INTO album (artist, title) VALUES ('The Military Wives', 'In My Dreams');
-INSERT INTO album (artist, title) VALUES ('Adele', '21');
-INSERT INTO album (artist, title) VALUES ('Bruce Springsteen', 'Wrecking Ball (Deluxe)');
-INSERT INTO album (artist, title) VALUES ('Lana Del Rey', 'Born To Die');
-INSERT INTO album (artist, title) VALUES ('Gotye', 'Making Mirrors');
