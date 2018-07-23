fe91e85dc9e9c6396cf1bfdb99ccb8426bd26750
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-07-23 18:15:18 +0300

    Added CircleCI to the project

--- config/constants.php
+++ config/constants.php
@@ -1,11 +1,12 @@
 <?php
 
 define("ROOT_FOLDER", __DIR__ . '/..');
+define("ENV", "development");
 
-if (file_exists(ROOT_FOLDER . '/config/dev.php')) {
-    define("ENV", "development");
-    ini_set('display_errors', 1);
-} else {
-    define("ENV", "production");
-    ini_set('display_errors', 0);
-}
+//if (file_exists(ROOT_FOLDER . '/config/dev.php')) {
+//    define("ENV", "development");
+//    ini_set('display_errors', 1);
+//} else {
+//    define("ENV", "production");
+//    ini_set('display_errors', 0);
+//}
