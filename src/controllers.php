23e6f5771a6af35dbc5c94924ff5a04a125a282c
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-08-09 20:36:41 +0300

    Added NLP to the application

--- src/controllers.php
+++ src/controllers.php
@@ -126,7 +126,8 @@ $app->post('/api/v1/webhook', function (Request $request) use ($app) {
         $webhookEvent   = $entry[0]['messaging'][0];
         $senderPsid     = $webhookEvent['sender']['id'];
         $app['monolog']->info(sprintf('Sender Psid: %s', $senderPsid));
-        $app['monolog']->info(var_export($nlp, true));
+        $app['monolog']->info(sprintf('NLP1: %s', var_export($nlp, true)));
+        $app['monolog']->info(sprintf('NLP2: %s', var_export($webhookEvent['message'], true)));
 
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
