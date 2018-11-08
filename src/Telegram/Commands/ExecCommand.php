a1b79eeea4c09196d1b421dbccef73868dff1c2e
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-11-08 17:49:54 +0200

    Added new log

--- src/Telegram/Commands/ExecCommand.php
+++ src/Telegram/Commands/ExecCommand.php
@@ -4,6 +4,7 @@ namespace Longman\TelegramBot\Commands\UserCommands;
 
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\KeyboardButton;
 use Longman\TelegramBot\Request;
 
 class ExecCommand extends UserCommand
@@ -15,10 +16,13 @@ class ExecCommand extends UserCommand
 
     public function execute()
     {
+        $button = new KeyboardButton('location');
+        $button->setRequestLocation(true);
+
         $data = [
             'chat_id'      => $this->getMessage()->getChat()->getId(),
             'text'         => 'Choose something',
-            'reply_markup' => new Keyboard(['Need some help', 'Who am I?']),
+            'reply_markup' => new Keyboard($button),
         ];
 
         return Request::sendMessage($data);
