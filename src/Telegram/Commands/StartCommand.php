316465058e8eb519a659cbe47d21f873a5453229
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-11-08 18:09:35 +0200

    Added new log

--- src/Telegram/Commands/StartCommand.php
+++ src/Telegram/Commands/StartCommand.php
@@ -1,62 +1,27 @@
 <?php
-/**
- * This file is part of the TelegramBot package.
- *
- * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
-namespace Bus115\Telegram\Commands;
+namespace Longman\TelegramBot\Commands\SystemCommands;
 
 use Longman\TelegramBot\Commands\SystemCommand;
 use Longman\TelegramBot\Request;
 
-/**
- * Start command
- *
- * Gets executed when a user first starts using the bot.
- */
 class StartCommand extends SystemCommand
 {
-    /**
-     * @var string
-     */
     protected $name = 'start';
-
-    /**
-     * @var string
-     */
     protected $description = 'Start command';
-
-    /**
-     * @var string
-     */
     protected $usage = '/start';
-
-    /**
-     * @var string
-     */
     protected $version = '1.1.0';
-
-    /**
-     * @var bool
-     */
     protected $private_only = true;
 
-    /**
-     * Command execute method
-     *
-     * @return \Longman\TelegramBot\Entities\ServerResponse
-     * @throws \Longman\TelegramBot\Exception\TelegramException
-     */
     public function execute()
     {
         $message = $this->getMessage();
 
-        $chat_id = $message->getChat()->getId();
-        $text    = 'Hi there!' . PHP_EOL . 'Type /help to see all commands!';
+        $chat_id    = $message->getChat()->getId();
+        $text       = 'Я чат-бот розкладу громадського транспорту Києва!'.PHP_EOL;
+        $text       .= 'Напишіть, звідки Ви вирушаєте - назву вулиці, провулку, площі тощо або назву зупинки. Або ж надішліть ваше місцезнаходження' .PHP_EOL;
+        $text       .= 'Для того, щоб надіслати місцезнаходження, напишіть /location' .PHP_EOL;
+        $text       .= 'Для того, щоб побачити перелік доступних команд, напишіть /help';
 
         $data = [
             'chat_id' => $chat_id,
