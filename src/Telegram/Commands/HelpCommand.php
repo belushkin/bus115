7db8a00e716ddf41c11857dccc43b9eb1590ed38
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-11-08 17:33:44 +0200

    Added new log

--- src/Telegram/Commands/HelpCommand.php
+++ src/Telegram/Commands/HelpCommand.php
@@ -4,6 +4,8 @@ namespace Longman\TelegramBot\Commands\UserCommands;
 
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Request;
+use Longman\TelegramBot\Entities\Keyboard;
+use Longman\TelegramBot\Entities\Update;
 
 class HelpCommand extends UserCommand
 {
@@ -16,11 +18,32 @@ class HelpCommand extends UserCommand
     {
         $message = $this->getMessage();
         $chat_id = $message->getChat()->getId();
+        $user_id = $message->getFrom()->getId();
+        $text    = trim($message->getText(true));
+
+//        $keyboard = new Keyboard([
+//            ['text' => 'test', '' /* I DONT KNOW HOW TO TELL TO EXECUTE /test COMMAND HERE */]
+//        ]);
+
+        if ($text === '') {
+            $data['text']           = 'Choose something';
+            $data['chat_id']        = $chat_id;
+            $data['reply_markup']   = new Keyboard(['Need some help', 'Who am I?']);
+            return Request::sendMessage($data);
+        }
+
+        $update = json_decode($this->update->toJson(), true);
+        if ($text === 'Need some help') {
+            $update['message']['text'] = '/help';
+            return (new HelpCommand($this->telegram, new Update($update)))->preExecute();
+        }
+
         $data = [
             'chat_id' => $chat_id,
             'text'    => 'Напиши назву вулиці де знаходиться зупинка, або напиши "де я".',
         ];
 
-        return Request::sendMessage($data);
+        return Request::emptyResponse();
+        //return Request::sendMessage($data);
     }
 }
