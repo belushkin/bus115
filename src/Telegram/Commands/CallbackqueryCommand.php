8df3f521c7165e5b5a16d559814959153c78f5db
Maxim Bielushkin <maxb@onehourtranslation.com>
2018-11-09 16:31:19 +0200

    Added CallbackQueryCommand

---
+++ src/Telegram/Commands/CallbackqueryCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\SystemCommands;
+
+use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Request;
+
+class CallbackqueryCommand extends SystemCommand
+{
+    protected $name = 'callbackquery';
+    protected $description = 'Reply to callback query';
+    protected $version = '1.1.1';
+
+    public function execute()
+    {
+        $callback_query    = $this->getCallbackQuery();
+        $callback_query_id = $callback_query->getId();
+        $callback_data     = $callback_query->getData();
+
+        $data = [
+            'callback_query_id' => $callback_query_id,
+            'text'              => 'Hello World!',
+            'show_alert'        => $callback_data === 'thumb up',
+            'cache_time'        => 5,
+        ];
+
+        return Request::answerCallbackQuery($data);
+    }
+}
